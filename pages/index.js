97d0c1c3bda881c9d92241045755f63f0a861b42
tahidur <tahidurrahman.tuhin@gmail.com>
2022-03-19 12:06:43 +0600

    signup design

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,12 @@ import styles from '../styles/Home.module.css'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css';
 import { userState } from 'react';
-import styled from "styled-components";
-
-const Input = styled.input.attrs(props => ({
-  type: "range",
-}))`
-  width: 65%;
-  float:right;
-  cursor: pointer;
-  &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 42px;
-    background: #4d4d4d;
-  }
-
-  &::-webkit-slider-thumb {
-    height: 43px;
-    top: 50%;
-    background: orange;
-  }
-`;
 
 export default function Home() {
-
-
 
   const  [verify, setVarify] = useState(false);
 
   const  [signup, setSignup] = useState(false);
-
 
   return (
     <div className={styles.container}>
@@ -41,7 +18,6 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
 
 
       <div className="max-w-2xl drop-shadow rounded-lg	mx-auto sm:px-6 lg:px-8 w-full">
@@ -60,33 +36,40 @@ export default function Home() {
                 <span className="flex items-center leading-normal bg-grey-lighter rounded rounded-r-none border border-r-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">880</span>
               </div>	
               <input type="text" name='phone' className="flex-shrink flex-grow leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-l-none px-3 relative focus:border-green-400" />
-            </div>		
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                    <button onClick={()=>{setVarify(true)}} className="font-bold text-green transition duration-300 ease-in-out mr-2">
+                        Verify
+                    </button>
 
+                </div>
 
-            <div className="flex flex-wrap items-stretch w-full mb-4 relative">
-            <span className='mx-9 pt-2'>Slide To Verify <span className='text-orange'>*</span></span>
-              <Input style={Home.notRobot} min="0" max="100" onChange={(e)=>{(e.target.value == 100)?setVarify(true):''}}/>
             </div>
 
 
 
           {verify && (<div className='text-center mt-8'>
 
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal mr-4 w-px border h-10 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal w-px mr-4 border h-10 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal w-px mr-4 border h-10 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal w-px border mr-4 h-10 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal w-px border h-10 mr-4 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
-            <input type="text" className="flex-shrink flex-grow flex-auto leading-normal w-px border h-10 mr-4 border-grey-light rounded rounded-l-none px-5 relative focus:border-green-400" />
 
-            <div className='text-center mt-8'>
-              <button onClick={()=>{setSignup(true)}} className="bg-orange-500 rounded-full font-bold text-white px-3 py-3 transition duration-300 ease-in-out mr-6">
-                  Confirm
-              </button>
 
-            </div>
+                  <div className="flex flex-wrap items-stretch w-full mb-4 relative">
+                  <span className='mx-9 pt-2'>Enter Code <span className='text-red'>*</span></span>
 
-            </div>
+                  <div className="flex -mr-px">
+                      <span className="flex items-center leading-normal bg-grey-lighter rounded rounded-r-none border border-r-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">880</span>
+                  </div>
+                  <input type="text" name='phone' className="flex-shrink flex-grow leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-l-none px-3 relative focus:border-green-400" />
+                  <div className="absolute inset-y-0 right-0 flex items-center">
+                      <button onClick={()=>{setSignup(true)}} className="font-bold text-green transition duration-300 ease-in-out mr-2">
+                          Confirm
+                      </button>
+
+                  </div>
+
+
+
+                  </div>
+
+              </div>
 
 
            )}
